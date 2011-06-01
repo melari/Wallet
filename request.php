ccfce743e8ffb320e2c89faa16922f555b2f1823
Caleb Simpson <the.foner@gmail.com>
2011-06-01 14:28:32 -0400

    Added security to request.php

--- request.php
+++ request.php
@@ -1,5 +1,15 @@
 <?php
-$filename="../files/" . $_GET['file'];
+include 'constants.php';
+include 'support.php';
+include 'secure.php';
+
+if (!isClean($_GET['file']))
+{
+	redirect("message.php?mes=Invalid file request.");
+	exit;
+}
+
+$filename= $__ARCHIVE_LOCATION . $_GET['file'];
 header("Content-disposition: attachment;filename=$filename");
 readfile($filename);
 ?>
