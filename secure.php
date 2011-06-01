edf4cf22ccbc9c7124dd2c4910d91a3ab16878e5
Caleb Simpson <the.foner@gmail.com>
2011-06-01 15:21:05 -0400

    Changed security string from local var to a constant.

--- secure.php
+++ secure.php
@@ -6,10 +6,10 @@ include 'support.php';
 
 //Checks that the client provided the correct passphrase to access Wallet with.
 $fh = fopen($__SECURE_LOCATION, 'r');
-$real = substr(fread($fh, filesize($__SECURE_LOCATION)), 0, 22);
+$__SECURE_STR = substr(fread($fh, filesize($__SECURE_LOCATION)), 0, 22);
 fclose($fh);
 
-if ($_GET['s'] != $real)
+if ($_GET['s'] != $__SECURE_STR)
 {
 	redirect("message.php?mes=Invalid security credentials.");
 }
