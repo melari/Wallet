a9ccbee60edb0b25203a5bb2bfdf8bf54d0a0ced
Caleb Simpson <the.foner@gmail.com>
2011-06-02 11:43:34 -0400

    Added 'no files found' message if the request finds no files.

--- htdocs/requestList.php
+++ htdocs/requestList.php
@@ -11,13 +11,19 @@ if (!isset($_GET['q']))
 if ($handle = opendir($__ARCHIVE_LOCATION))
 {
 	echo("Matched Files:<br />\n");
+	$count = 0;
 	while (false !== ($file = readdir($handle))) 
 	{
     	if (startsWith($file, $_GET['q']))
 		{
+			$count++;
 			echo("<a href='http://" . $__LOCAL_IP . "/request.php?file=" . $file . "&s=" . $__SECURE_STR . "'>" . $file . "</a><br />\n");
 		} 
     }
+	if ($count == 0)
+	{
+		echo($__NO_FILES_FOUND);
+	}
 }
 else
 {
